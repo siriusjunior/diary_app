960ef8fd5543f5642f435b208bd9dd0e68f738d8
siriusjunior <topdownreading@gmail.com>
2021-10-20 10:01:58 +0900

    i18nの適用

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,18 @@ module DiaryApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    # デフォルトのlanguageを:jaに設定
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
 
     config.generators do |g|
       g.assets  false # CSS, JSが自動生成されない
       g.test_framework  false # Minitestが自動生成されない
       g.skip_routes  true # ルーティングが自動生成されない
     end
-    
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
