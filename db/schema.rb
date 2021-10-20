16dcfe0c4ec0e67588391bec2accdf7ae9a7ab28
siriusjunior <topdownreading@gmail.com>
2021-10-20 17:22:08 +0900

    ログイン状態の保持ができるように実装した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_203553) do
+ActiveRecord::Schema.define(version: 2021_10_20_074500) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 2021_10_19_203553) do
     t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
   end
 
 end
