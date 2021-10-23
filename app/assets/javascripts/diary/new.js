87c5fa79b962fe81b700c597cb94f5dac7d91fe1
siriusjunior <topdownreading@gmail.com>
2021-10-24 06:21:20 +0900

    [Add]Diary新規作成時のCarrierWave画像プレビュー機能をjsで実装

--- app/assets/javascripts/diary/new.js
+++ app/assets/javascripts/diary/new.js
@@ -3,3 +3,21 @@
 //= require rails-ujs
 //= require activestorage
 //= require bootstrap-material-design/dist/js/bootstrap-material-design.js
+
+// function previewDiaryImageFile(preview){
+//     // inputタグの取得
+//     const target = this.event.target;
+//     // ファイル内容の取得
+//     const file = target.files[0];
+//     // ファイルを読み込むためのインスタンスを生成
+//     const reader = new FileReader();
+//     // ファイル内容を取得したので、URLをファイルから取得してリーダーに格納
+//     if (file) {
+//         reader.readAsDataURL(file);
+//     }
+//     // ファイルリーダーで読み込んだら(onload)、要素のsrcをreaderのURLで書き換える
+//     reader.onloadend = function (){
+//         $('#preview').attr('src', reader.result);
+//     }
+    
+// }
