765b76a37d9aff01a16c11732dd5cf2569119f32
siriusjunior <topdownreading@gmail.com>
2021-11-01 21:39:55 +0900

    [Modify]非ログインユーザーのアクセス制御のflash更新、_diaryパーシャルの更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
     private
         def not_authenticated
-            redirect_to login_path, alert: 'ログインをしてください'
+            redirect_to login_path, danger: "ログインしてください"
         end
 end
