3ab418cd931573bf99b64dc17caad5d12168f536
siriusjunior <topdownreading@gmail.com>
2021-11-03 06:39:58 +0900

    [Add]ページネーションのビュー反映、位置調整

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  config.default_per_page = 15
-  config.max_per_page = 15
+  config.default_per_page = 10
+  config.max_per_page = 10
   config.window = 3
   # config.outer_window = 0
   # config.left = 0
