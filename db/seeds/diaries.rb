3eac6da7b34323b660f21593e23141e045e43662
siriusjunior <topdownreading@gmail.com>
2021-11-04 10:06:24 +0900

    [Modify]seedファイルにダミー画像が反映されるようにした

--- db/seeds/diaries.rb
+++ db/seeds/diaries.rb
@@ -1,9 +1,18 @@
 Faker::Config.locale = :en
 puts 'Start inserting seed "diaries" ...'
-User.limit(10).each do |user|
+User.limit(13).each do |user|
     diary = user.diaries.create(
-        body: Faker::Lorem.paragraph(sentence_count: 15, supplemental: false, random_sentences_to_add: 13),
-        date_sequence: 1
+        body: Faker::Lorem.paragraph(sentence_count: 8, supplemental: false, random_sentences_to_add: 5),
+        date_sequence: 1,
+        remote_image_url: "https://picsum.photos/700/700/?random",
+        check: Faker::Lorem.paragraph(sentence_count: 3, supplemental: false, random_sentences_to_add: 3)
     )
-    puts "diary#{diary.id} has created!"
+    puts "day1 diary#{diary.id} has created!"
+    diary = user.diaries.create(
+        body: Faker::Lorem.paragraph(sentence_count: 8, supplemental: false, random_sentences_to_add: 5),
+        date_sequence: 2,
+        remote_image_url: "https://picsum.photos/700/700/?random",
+        check: Faker::Lorem.paragraph(sentence_count: 3, supplemental: false, random_sentences_to_add: 3)
+    )
+    puts "day2 diary#{diary.id} has created!"
 end
