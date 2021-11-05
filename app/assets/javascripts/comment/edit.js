7af84b5be15394b9be557d069fe082f6c03df29a
siriusjunior <topdownreading@gmail.com>
2021-11-05 10:37:37 +0900

    [Modify]assets/javascripts内の不要箇所を削除

--- app/assets/javascripts/comment/edit.js
+++ app/assets/javascripts/comment/edit.js
@@ -1,9 +1,3 @@
-//= require jquery3
-//= require popper
-//= require rails-ujs
-//= require activestorage
-//= require bootstrap-material-design/dist/js/bootstrap-material-design.js
-
 function show_comment_edit(){
     $('#comment-edit-form').addClass('d-block').removeClass('d-none');
 }
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # 挙動調整のため追記するかも、ページでjavascriptを読み込むための設定
+Rails.application.config.assets.precompile += %w( *.js )
 # Rails.application.config.assets.precompile += ['*.js']
