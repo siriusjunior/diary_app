cd7e2c7874975eeb881616d4fa599614070d134f
siriusjunior <topdownreading@gmail.com>
2021-11-06 20:56:31 +0900

    [Add]モデルにフォローメソッドの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,4 +71,20 @@ class User < ApplicationRecord
     like_diaries.include?(diary)
   end
 
+  def follow(other_user)
+    following << other_user
+  end
+  
+  def unfollow(other_user)
+    following.destroy(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  def feed
+    Post.where(user_id: following_ids << id)
+  end
+
 end
