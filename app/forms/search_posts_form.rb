e3eaee3c01c1bf247b2669e6f270b618e25d5b43
siriusjunior <topdownreading@gmail.com>
2021-11-11 05:58:45 +0900

    [Add]ダイアリー検索メソッドの作成

--- app/forms/search_posts_form.rb
+++ app/forms/search_posts_form.rb
@@ -4,4 +4,16 @@ class SearchPostsForm
 
     attribute :body, :string
 
+    def search
+        scope = Diary.distinct
+        # inject以下はscopeをorでチェーン
+        scope = splited_bodies.map { |splited_body| scope.body_contain(splited_body) }.inject{ |result, scp| result.or(scp) } if body.present?
+        scope
+    end
+
+    private
+
+        def splited_bodies
+            body.strip.split(/[[:blank:]]+/)
+        end
 end
