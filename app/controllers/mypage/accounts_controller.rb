6d61f03081cb5245035e84347ea87a2a3f8060a4
siriusjunior <topdownreading@gmail.com>
2021-11-12 05:50:09 +0900

    [Fix]プロフ編集でパスワードエラーになったのでskip_passwordで回避

--- app/controllers/mypage/accounts_controller.rb
+++ app/controllers/mypage/accounts_controller.rb
@@ -5,8 +5,9 @@ class Mypage::AccountsController < Mypage::BaseController
 
   def update
     @user = User.find(current_user.id)
+    @user.skip_password = true
     if @user.update(account_params)
-      redirect_to edit_mypage_account_path, success: 'プロフィールを更新しました'
+      redirect_to edit_mypage_account_path, info: 'プロフィールを更新しました'
     else
       flash.now[:danger] = 'プロフィールの更新に失敗しました'
       render :edit
