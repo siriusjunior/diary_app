9c0dd4202ff3d46ccfd8f23cacd53050a741a34a
siriusjunior <topdownreading@gmail.com>
2021-11-12 06:06:05 +0900

    [Add]アカウント編集ページでプロフィール画像を読み取ったらプレビューできるようにした

---
+++ app/assets/javascripts/mypage.js
@@ -0,0 +1,21 @@
+//= require jquery3
+//= require popper
+//= require rails-ujs
+//= require bootstrap-material-design/dist/js/bootstrap-material-design.js
+
+function previewFileWithId(selector) {
+    const target = this.event.target;
+    const file = target.files[0];
+    // ファイルを読み込むためのインスタンスを生成
+    const reader  = new FileReader()
+    // ファイルリーダーで読み込んだら(onload)、要素のsrcをreaderのURLで書き換える;
+    reader.onloadend = function () {
+        selector.src = reader.result;
+    }
+    if (file) {
+        // ファイル内容を取得したので、URLをファイルから取得してリーダーに格納
+        reader.readAsDataURL(file);
+    } else {
+        selector.src = "";
+    }
+}
