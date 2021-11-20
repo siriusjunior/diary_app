b7eaa386ea956c6277bc32361d90f602602e5249
siriusjunior <topdownreading@gmail.com>
2021-11-21 05:20:06 +0900

    [Add]action_typeに対応するdefault_valueが求められたのでActivity.rbに追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,16 @@
 //= require_tree .
 
 // console.log("呼ばれてる？？？");
+
+$(document).on('click',function(e) {
+    if(!$(e.target).closest('#dropdown').length) {
+      // 外側クリック
+    $('#header-activities').removeClass("show");
+    } else {
+    // ターゲットのクリック
+    if ($('#header-activities').hasClass('show')){
+        $('#header-activities').removeClass("show");
+    }else{
+        $('#header-activities').addClass("show");
+    }}
+});
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -26,6 +26,7 @@ class Activity < ApplicationRecord
 
   scope :recent, ->(count){ order(created_at: :desc).limit(count) }
 
+  enum action_type: { commented_to_own_post: 0, liked_to_own_post: 1, followed_me: 2 }
   enum read: { unread: false, read: true }
 
   def redirect_path
