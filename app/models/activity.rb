256c81642e5b3836aa1e25b845df3313920b6931
siriusjunior <topdownreading@gmail.com>
2021-11-20 12:08:58 +0900

    [Add]ヘッダー通知dropdownボタンと通知表示のパーシャル追記

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,28 @@
+# == Schema Information
+#
+# Table name: activities
+#
+#  id           :bigint           not null, primary key
+#  action_type  :integer          not null
+#  read         :boolean          default(FALSE), not null
+#  subject_type :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  subject_id   :bigint
+#  user_id      :bigint
+#
+# Indexes
+#
+#  index_activities_on_subject_type_and_subject_id  (subject_type,subject_id)
+#  index_activities_on_user_id                      (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Activity < ApplicationRecord
   belongs_to :subject, polymorphic: true
   belongs_to :user
+
+  scope :recent, ->(count){ order (created_at: :desc).limit(count) }
 end
