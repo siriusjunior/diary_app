c2ff58b37cdd4ab07f192e95b0fe1f049809ac99
siriusjunior <topdownreading@gmail.com>
2021-11-23 05:41:17 +0900

    [Add]通知設定のyml追加,[Fix]params設定のpermitが抜けていたので追加

--- app/controllers/mypage/notification_settings_controller.rb
+++ app/controllers/mypage/notification_settings_controller.rb
@@ -5,6 +5,7 @@ class Mypage::NotificationSettingsController < Mypage::BaseController
 
   def update
     @user = User.find(current_user.id)
+    @user.skip_password = true
     if @user.update(notification_setting_params)
       redirect_to edit_mypage_notification_setting_path, success: '通知設定を更新しました'
     else
@@ -16,7 +17,7 @@ class Mypage::NotificationSettingsController < Mypage::BaseController
   private
 
     def notification_setting_params
-      params.require(:user).(:notification_on_comment, :notification_on_like, :notification_on_follow, :notification_on_comment_like)
+      params.require(:user).permit(:notification_on_comment, :notification_on_like, :notification_on_follow, :notification_on_comment_like)
     end
     
 end
