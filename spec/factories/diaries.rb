c291ed107db17375ef25859c882ac276df5e3928
siriusjunior <topdownreading@gmail.com>
2021-11-26 05:56:06 +0900

    [Add]FactoryBotの設定追加

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :diary do
-    
+    body { Faker::Hacker.say_something_smart }
+    image { [File.open("#{Rails.root}/spec/fixtures/dummy.png")] }
+    user
   end
 end
