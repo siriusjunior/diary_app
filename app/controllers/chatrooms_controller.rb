a2f619fe8695be28505f9d55452b51a2a23c82ab
siriusjunior <topdownreading@gmail.com>
2021-12-06 12:03:21 +0900

    [Add]ユーザー詳細ページのボタンからチャットルーム作成機能

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,12 +1,23 @@
 class ChatroomsController < ApplicationController
-  before_action :require_login, only: %i[show]
+  before_action :require_login, only: %i[index create show]
+  before_action :require_user_ids, only: %i[create]
   def index
   end
 
   def create
+    users = User.where(id: params.dig(:chatroom :user_ids)) + [current_user]
+    @chatroom = Chatroom.chatroom_with_users(users)
+    redirect_to chatroom_path(@chatroom)
   end
   
   def show
     @chatroom = current_user.chatrooms.find(params[:id])
   end
+
+  private
+
+    def require_user_ids
+      redirect_back(fallback_location: root_path, danger: 'パラメーターが不正です') if params.dig(:chatroom, :user_ids).reject(&:blank?).blank?
+    end
+
 end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -11,4 +11,16 @@ class Chatroom < ApplicationRecord
     has_many :chatroom_users, dependent: :destroy
     has_many :users, through: :chatroom_users
     has_many :messages, dependent: :destroy
+
+    def self.chatroom_with_users(users)
+        # [1,4,6]
+        user_ids = users.map(&:id).sort
+        name = user_ids.join(':').to_s
+        unless(chatroom = find_by(name: name))
+            chatroom = new(name: name)
+            chatroom.users = users
+            chatroom.save
+        end
+        chatroom
+    end
 end
