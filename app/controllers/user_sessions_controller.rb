043a69896d221f70c2c776ed6da0262827c93ebc
siriusjunior <topdownreading@gmail.com>
2021-12-06 16:23:42 +0900

    [Add]ActionCableの認証処理を追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,9 +5,13 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password], params[:remember])
     if @user
       if session[:return_to]
+        # ActionCableの認証処理に必要なcookies
+        cookies.signed['user.id'] = current_user.id
         redirect_to session[:return_to], success: "ログインしました"
         session.delete(:return_to)
       else
+        # ActionCableの認証処理に必要なcookies
+        cookies.signed['user.id'] = current_user.id
         redirect_back_or_to diaries_url, success: "ログインしました"
       end
     else
@@ -18,6 +22,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
+    cookies.delete('user.id')
     redirect_back_or_to root_path, success: "ログアウトしました"
   end
 
