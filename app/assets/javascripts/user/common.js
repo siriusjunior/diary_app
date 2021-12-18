ac675233fd3f2bb0be7c71dff1bacd1eb6071216
siriusjunior <topdownreading@gmail.com>
2021-12-18 12:49:55 +0900

    [Modify]ユーザーのfollowing/followers一覧の_user内タグリンク無効化

---
+++ app/assets/javascripts/user/common.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('#user-follower .tag').click(function(){
+        return false;
+    });
+});
+$(function(){
+    $('#user-following .tag').click(function(){
+        return false;
+    });
+});
