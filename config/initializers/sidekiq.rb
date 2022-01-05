3f111fc32c81c933eed94e312141b5cee52d5eaf
siriusjunior <topdownreading@gmail.com>
2022-01-06 06:16:24 +0900

    Sidekiqの設定をビルド寄りに変更

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,15 @@
 Sidekiq.configure_server do |config|
     config.redis = {
-        url: 'redis://redis:6379'
+        url: 'redis://localhost:6379'
+        #redisコンテナではなくビルド用に上記に書換え
+        # url: 'redis://redis:6379'
     }
 end
 
 Sidekiq.configure_client do |config|
     config.redis = {
-        url: 'redis://redis:6379'
+        url: 'redis://localhost:6379'
+        #redisコンテナではなくビルド用に上記に書換え
+        # url: 'redis://redis:6379'
     }
 end
