7f9f24b1f64de718609618935ef1754769cf7997
siriusjunior <topdownreading@gmail.com>
2022-01-06 06:09:40 +0900

    Redisのconnection設定変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,8 @@
 Rails.application.config.session_store :redis_store, {
     servers: [
         {
-            host: "redis",  #Redisのサーバー名
+            host: "localhost",  #Redisのサーバー名
+            # host: "redis",  #Redisのサーバー名 redisコンテナではなくビルド用に上記に書換え
             port: 6379, #Redisのサーバーのポート
             db: 0, #データーベースの番号(0~15)任意
             namespace: "session" #名前空間,"session:セッションID"の形式
