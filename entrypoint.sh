4c77798136bf2caf819b042ee8c142428f440741
siriusjunior <topdownreading@gmail.com>
2022-01-29 11:36:27 +0900

    [Modify]entrypoint.shの修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,16 @@
 set -e
 # 1L(スクリプトを/bin/bashで解釈し処理進行),2L(Bashの内部コマンド、シェルオプション値,位置パラメータ設定)
 # Rails対応ファイルserver.pidを削除
-rm -f /app/tmp/pids/server.pid
+rm -f tmp/pids/server.pid
+
 # create,seedはfargate初回のみ実行,以降は書換え配備
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
+bundle exec rails assets:precompile
+
+# 初回のみ実行
+# mkdir tmp/sockets
+# touch tmp/sockets/puma.sock
 # コンテナプロセス実行(DockerfileのCMD,参考(https://qiita.com/RiSE_blackbird/items/e16c84cc18d4639d5d4f)）
 exec "$@"
