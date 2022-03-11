9e1ae70f5af092bfbb695c473d9c197524e8d493
siriusjunior <topdownreading@gmail.com>
2022-03-11 11:32:47 +0900

    [Fix]config.time_zone指定を解除してplanモデルとmessageモデルの不整合を解消

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module DiaryApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Tokyo'
+    #以下のtime_zoneで、plan.rbのTime.currentがJSTになるが、messageモデルのcreated_atがUTCで不整合が起きるため指定しない
+    #config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     # デフォルトのlanguageを:jaに設定
     config.i18n.default_locale = :ja
